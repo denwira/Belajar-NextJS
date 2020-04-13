8912b6eae4388ae51bc2bfd585871e55af06700f
denwira <denrwijaya@gmail.com>
2020-04-14 02:07:28 +0700

    added Post Page

---
+++ pages/Post/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Post extends Component {
+	render() {
+		return <h1>Test</h1>;
+	}
+}
+
+export default Post;
